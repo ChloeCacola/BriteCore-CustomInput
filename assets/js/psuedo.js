97f8edebd5a52a51fe13f373b14b5d8c72e45d54
ChloeCacola <cscorrigan@icloud.com>
2018-01-10 00:10:05 -0500

    basic pseudo js laid out

---
+++ assets/js/psuedo.js
@@ -0,0 +1,40 @@
+//pseudo code for js
+
+
+//BASIC****
+/*
+SELECT A TYPE
+
+Selecting a FIELD TYPE will manipulate the Field Data Input to have specified HTML TYPES and OTHER CHANGES related to the change in input
+e.g. 'Currency' => type='text' + '$' included
+*/
+
+/*
+ON BLUR EVENTS
+
+1. Reference name auto-generated from user-entered label **camelCase**
+
+2. Custom validation true or false
+*/
+
+/*
+VALIDATE REGEX
+
+Onblur, check if user's custom regex is actually a regex pattern (boolean)
+
+*/
+
+
+//VUE CODE****
+/*
+FILTER
+
+Typing in the search box eliminates components that do not match based on TITLE (e.g. 'Text')
+*/
+
+/*
+TAGS
+
+Selecting a specified TAG GROUP (button) will generate TAG LIST
+*/
+
